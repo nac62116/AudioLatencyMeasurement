d9ded26de19b8d0937ca3753d3daf7453ed6a3d3
C N <nac62116@github.com>
2022-02-16 16:02:11 +0100

    Added bitstream version prototype

--- line_out_line_in/line_out_line_in.c
+++ line_out_line_in/line_out_line_in.c
@@ -1,55 +1,282 @@
 #include <pigpio.h>
+#include <stdio.h>
+
+/* Versions:
+ * v1.1: Iterative measurement, condition = no noise through signal loss/add
+ * --- Pros: Very Fast (TOTAL_MEASUREMENTS / SIGNAL_FREQUENCY_IN_HZ + latency of the DUT in seconds; Unit = seconds)
+ * --- Cons: Invalid measurements by losing/adding a signal through random noise
+ * 
+ * v1.2: Iterative measurement, condition = measurement frequency > latency of the DUT
+ * --- Pros: No invalidation of measurements by losing/adding a signal through random noise
+ * --- Cons: Rather slow (TOTAL_MEASUREMENTS * MEASUREMENT_FREQUENCY_IN_S, where MEASUREMENT_FREQUENCY_IN_SECONDS > latency of the DUT in seconds; Unit = seconds)
+ * --------: To know the right value for MEASUREMENT_FREQUENCY_IN_S one got to know/estimate the value of the latency of the DUT
+ * 
+ * v1.2.1: Iterative measurement, condition = measurement frequency > latency of DUT only for calibration measurements
+ * --- Pros: No invalidation of measurements by losing/adding a signal through random noise
+ * --------: No need to know/estimate the latency of the DUT
+ * --------: Reducing time in comparance to v1.2 by starting with a high MEASUREMENT_FREQUENCY_IN_S (above all DUT latencies)
+ *           and then reducing it after TOTAL_CALIBRATION_MEASUREMENTS to maxLatency + MEASUREMENT_FREQUENCY_MARGIN
+ * --- Cons: Still slower than v1.1
+ * 
+ * v2.1: Recursive measurement, condition = no noise through signal loss/add
+ * v2.2: TODO: Recursive measurement, condition = no noise through signal add while signal loss is recognized by a timeout
+ * --- Pros: Faster than v1.2 and v1.2.1
+ * --------: No need to know/estimate the latency of the DUT
+ * --- Cons: Invalid measurements by losing/adding a signal through random noise
+ * --------: Slower than v1.1
+ */
+
+
+// Constants
+#define LINE_OUT 17
+#define LINE_IN 27
+#define TOTAL_MEASUREMENTS 10
+#define SIGNAL_FREQUENCY_IN_HZ 200.0
+#define HALF_WAVELENGTH_IN_S (1.0 / SIGNAL_FREQUENCY_IN_HZ / 2.0)
+
+// v1.2: Iterative measurement, condition = measurement frequency > latency of DUT
+#define MEASUREMENT_FREQUENCY_IN_S 1.0
+
+// v1.2.1: Iterative measurement, condition = measurement frequency > latency of DUT only for calibration measurements
+#define TOTAL_CALIBRATION_MEASUREMENTS 100
+#define MEASUREMENT_FREQUENCY_MARGIN_IN_SECONDS 0.01
+
+// v2.1: Recursive measurement, condition = no noise through signal loss/add
+// v2.2: TODO: Recursive measurement, condition = no noise through signal add while signal loss is recognized by a timeout
+#define RUNNING 0
+#define FINISHED 1
+
+
+// Variables
+
+// v1.1: Iterative measurement, condition = no noise through signal loss/add
+uint32_t startTicks[TOTAL_MEASUREMENTS];
+uint32_t endTicks[TOTAL_MEASUREMENTS];
+int outCount = 0;
+int inCount = 0;
+
+// v1.2: Iterative measurement, condition = measurement frequency > latency of DUT
+uint32_t startTick, endTick;
+
+int latencyInMicros;
+int validMeasurmentsCount = 0;
+int latencyMeasurementsInMicros[TOTAL_MEASUREMENTS];
+int maxLatencyInMicros = -1;
+int minLatencyInMicros = -1;
+int sumOfLatenciesInMicros = 0;
+int avgLatencyInMicros;
+int status;
+
+// v2.1: Recursive measurement, condition = no noise through signal loss/add
+// v2.2: TODO: Recursive measurement, condition = no noise through signal add while signal loss is recognized by a timeout
+int measurementStatus;
+
+
+// Line-out callback
+void onLineOut(int gpio, int level, uint32_t tick) {
+    printf("GPIO %d state changed to level %d at %d\n", gpio, level, tick);
+    
+    if (level == 1) {
+        
+        // v1.1: Iterative measurement, condition = no noise through signal loss/add
+        startTicks[outCount] = tick;
+        
+        // v1.2: Iterative measurement, condition = measurement frequency > latency of DUT
+        //startTick = tick;
+        
+        outCount += 1;
+    }
+}
+
+// Line-in callback
+void onLineIn(int gpio, int level, uint32_t tick) {
+    printf("GPIO %d state changed to level %d at %d\n", gpio, level, tick);
+    
+    if (level == 1) {
+        
+        // v1.1: Iterative measurement, condition = no noise through signal loss/add
+        endTicks[inCount] = tick;
+        
+        // v1.2: Iterative measurement, condition = measurement frequency > latency of DUT
+        //endTick = tick;
+        
+        // Calculating latency in micros
+        // v1.1: Iterative measurement, condition = no noise through signal loss/add
+        latencyInMicros = endTicks[inCount] - startTicks[inCount];
+        
+        // v1.2: Iterative measurement, condition = measurement frequency > latency of DUT
+        //latencyInMicros = endTick - startTick;
+        
+        // The uint32_t tick parameter represents the number of microseconds since boot.
+        // This wraps around from 4294967295 to 0 roughly every 72 minutes.
+        // Thats why the provided latency could be both negative and wrong in this specific situation.
+        if (latencyInMicros >= 0) {
+            validMeasurmentsCount += 1;
+            
+            // Saving valid measurement
+            latencyMeasurementsInMicros[inCount] = latencyInMicros;
+            printf("The signal had a latency of %d microseconds\n", latencyInMicros);
+            
+            // Calculating running descriptive values (min, max, avg)
+            if (maxLatencyInMicros == -1) {
+                maxLatencyInMicros = latencyInMicros;
+            }
+            else if (latencyInMicros > maxLatencyInMicros) {
+                maxLatencyInMicros = latencyInMicros;
+            }
+            else {}
+            
+            if (minLatencyInMicros == -1) {
+                minLatencyInMicros = latencyInMicros;
+            }
+            else if (latencyInMicros >= 0 && latencyInMicros < minLatencyInMicros) {
+                minLatencyInMicros = latencyInMicros;
+            }
+            else {}
+            
+            sumOfLatenciesInMicros += latencyInMicros;
+            
+            avgLatencyInMicros = sumOfLatenciesInMicros / validMeasurmentsCount;
+        }
+        inCount += 1;
+    }
+    else {
+        
+        /* v2.1: Recursive measurement, condition = no noise through signal loss/add
+        // v2.2: TODO: Recursive measurement, condition = no noise through signal add while signal loss is recognized by a timeout
+        
+        // Start next measurement or finish
+        if (inCount < TOTAL_MEASUREMENTS) {
+            // Send signal through output (3.3V square wave, measurement frequency = latency of DUT)
+            printf("\n\n----- Measurement %d started -----\n", outCount + 1);
+            status = gpioWrite(LINE_OUT, 1);
+            printf("status (0 = OK; <0 = ERROR): %d\n", status);
+            time_sleep(HALF_WAVELENGTH_IN_S);
+            status = gpioWrite(LINE_OUT, 0);
+            printf("status (0 = OK; <0 = ERROR): %d\n", status);
+        }
+        else {
+            measurementStatus = FINISHED;
+        }
+        */
+    }
+}
 
 int main(void) {
-    const int OUTPUT_GPIO_17 = 17;
-    const int INPUT_GPIO_27 = 27;
-
-    uint32_t startTick, endTick;
-    int latencyInMicros;
-    int measurementCount = 0;
-    int latencyMeasurementsInMicros[10000];
-
+    int status/*, cfg*/;
+    
+    gpioTerminate();
     /* Documentation (https://abyz.me.uk/rpi/pigpio/cif.html):
     "If you intend to rely on signals sent to your application,
     you should turn off the internal signal handling [...]:"
-    */
-    int cfg = gpioCfgGetInternals();
+    
+    cfg = gpioCfgGetInternals();
     cfg |= PI_CFG_NOSIGHANDLER;  // (1<<10)
-    gpioCfgSetInternals(cfg);
+    gpioCfgSetInternals(cfg);*/
 
     // Initialize library
-    gpioInitialise();
+    status = gpioInitialise();
+    //printf("Status after gpioInitialise: %d\n", status);
 
     // Set GPIO Modes
-    gpioSetMode(OUTPUT_GPIO_17, PI_OUTPUT);
-    gpioSetMode(INPUT_GPIO_27, PI_INPUT);
+    gpioSetMode(LINE_OUT, PI_OUTPUT);
+    gpioSetMode(LINE_IN, PI_INPUT);
 
     // Register GPIO state change callback
-    gpioSetAlertFunc(OUTPUT_GPIO_17, onStateChanged);
-    gpioSetAlertFunc(INPUT_GPIO_27, onStateChanged);
+    gpioSetAlertFunc(LINE_OUT, onLineOut);
+    gpioSetAlertFunc(LINE_IN, onLineIn);
+    
 
-    // Send signal through output (3.3V square wave)
-    gpioWrite(OUTPUT_GPIO_17, 1);
-    gpioWrite(OUTPUT_GPIO_17, 0);
-}
-
-// GPIO state changed
-void onStateChanged(int gpio, int level, uint32_t tick) {
-    if (level == 1) {
-        if (gpio == OUTPUT_GPIO_17) {
-            startTick = tick;
+    // Iterative measurement
+    
+    for (int i = 0; i < TOTAL_MEASUREMENTS; i++) {
+        
+        
+        /* v1.2.1: Iterative measurement, condition = measurement frequency > latency of DUT only for calibration measurements
+        
+        if (i == TOTAL_CALIBRATION_MEASUREMENTS) {
+            measurementFrequencyInSeconds = maxLatencyInMicros / 1000000 + MEASUREMENT_FREQUENCY_MARGIN_IN_SECONDS;
         }
-        if (gpio == INPUT_GPIO_27) {
-            endTick = tick;
-            latencyInMicros = endTick - startTick;
-            // The uint32_t tick parameter represents the number of microseconds since boot.
-            // This wraps around from 4294967295 to 0 roughly every 72 minutes.
-            // Thats why the provided latency could be both negative and wrong in this specific situation
-            if (latencyInMicros >= 0) {
-                latencyMeasurementsInMicros[measurementCount] = latencyInMicros;
-                printf("The signal had a latency of %d microseconds", latencyInMicros);
-                measurementCount += 1;
-            }
-        }
+        */
+        
+        
+        /* v1.1: Iterative measurement, condition = no noise through signal loss/add
+        
+        // Send signal through output (3.3V square wave, measurement frequency = SIGNAL_FREQUENCY_IN_HZ)
+        printf("\n\n----- Measurement %d started -----\n", i + 1);
+        status = gpioWrite(LINE_OUT, 1);
+        printf("status (0 = OK; <0 = ERROR): %d\n", status);
+        time_sleep(HALF_WAVELENGTH_IN_S);
+        status = gpioWrite(LINE_OUT, 0);
+        printf("status (0 = OK; <0 = ERROR): %d\n", status);
+        time_sleep(HALF_WAVELENGTH_IN_S);
+        */
+        
+        // v3: Bitstream measurement: Test Signal (110 | 100 | 10); bitFrequency = HALF_WAVELENGTH_IN_S
+        printf("\n\n----- Measurement %d started -----\n", i + 1);
+        // 110
+        status = gpioWrite(LINE_OUT, 1);
+        time_sleep(HALF_WAVELENGTH_IN_S * 2.0);
+        status = gpioWrite(LINE_OUT, 0);
+        time_sleep(HALF_WAVELENGTH_IN_S);
+        // bitstream end signal
+        status = gpioWrite(LINE_OUT, 0);
+        time_sleep(HALF_WAVELENGTH_IN_S * 0.5);
+        status = gpioWrite(LINE_OUT, 1);
+        time_sleep(HALF_WAVELENGTH_IN_S * 0.5);
+        status = gpioWrite(LINE_OUT, 0);
+        time_sleep(HALF_WAVELENGTH_IN_S * 0.5);
+        // 101
+        status = gpioWrite(LINE_OUT, 1);
+        time_sleep(HALF_WAVELENGTH_IN_S);
+        status = gpioWrite(LINE_OUT, 0);
+        time_sleep(HALF_WAVELENGTH_IN_S);
+        status = gpioWrite(LINE_OUT, 1);
+        time_sleep(HALF_WAVELENGTH_IN_S);
+        // bitstream end signal
+        status = gpioWrite(LINE_OUT, 0);
+        time_sleep(HALF_WAVELENGTH_IN_S * 0.5);
+        status = gpioWrite(LINE_OUT, 1);
+        time_sleep(HALF_WAVELENGTH_IN_S * 0.5);
+        status = gpioWrite(LINE_OUT, 0);
+        time_sleep(HALF_WAVELENGTH_IN_S * 0.5);
+        
+        
+        // v1.2: Iterative measurement, condition = measurement frequency > latency of DUT
+        // v1.2.1: Iterative measurement, condition = measurement frequency > latency of DUT only for calibration measurements
+        
+        // Measurement frequency = MEASUREMENT_FREQUENCY_IN_S
+        //time_sleep(MEASUREMENT_FREQUENCY_IN_S);
     }
+    printf("\n%d\n", status);
+    
+    /* v2.1: Recursive measurement, condition = no noise through signal loss/add
+    // v2.2: TODO: Recursive measurement, condition = no noise through signal add while signal loss is recognized by a timeout
+    
+    // Send signal through output (3.3V square wave, measurement frequency = latency of DUT)
+    measurementStatus = RUNNING;
+    printf("\n\n----- Measurement %d started -----\n", outCount + 1);
+    status = gpioWrite(LINE_OUT, 1);
+    printf("status (0 = OK; <0 = ERROR): %d\n", status);
+    time_sleep(HALF_WAVELENGTH_IN_S);
+    status = gpioWrite(LINE_OUT, 0);
+    printf("status (0 = OK; <0 = ERROR): %d\n", status);
+    
+    // Waiting for measurement series to finish
+    while (measurementStatus == RUNNING) {
+        time_sleep(1);
+    }
+    */
+    
+    // Print measurements
+    for (int i = 0; i < TOTAL_MEASUREMENTS; i++) {
+        
+        printf("\n##### Measurement %d latency: %d\n", i + 1, latencyMeasurementsInMicros[i]);
+    }
+    
+    gpioTerminate();
+    
+    // TODO: Saving measurements to .csv format
+    
+    printf("\nExit\n");
 }
