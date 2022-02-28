d58fbc4549019ea14f320756e7e990e2bc553520
nac62116 <cnash@cnash.de>
2022-02-28 14:53:51 +0100

    Get hardware parameters

--- latency_measurement/alsa_out.c
+++ latency_measurement/alsa_out.c
@@ -4,12 +4,62 @@ Code base retrieved from https://www.alsa-project.org/alsa-doc/alsa-lib/_2test_2
 
 #include <alsa/asoundlib.h>
 
-static char *device = "hw:1,0";                        /* USB playback device */
+static char *device = "hw:1,0";          /* USB playback device */
 //static char *device = "hw:2,0";        /* HDMI 1 playback device */
 //static char *device = "hw:0,0";        /* HDMI 0 playback device */
 
 snd_output_t *output = NULL;
 unsigned char buffer[16*1024];                          /* some random data */
+
+void displayHardwareParameters() {
+    /* Display information about the PCM interface */
+
+    printf("PCM handle name = '%s'\n",
+            snd_pcm_name(handle));
+
+    printf("PCM state = %s\n",
+            snd_pcm_state_name(snd_pcm_state(handle)));
+
+    snd_pcm_hw_params_get_access(params,
+            (snd_pcm_access_t *) &val);
+    printf("access type = %s\n",
+            snd_pcm_access_name((snd_pcm_access_t)val));
+
+    snd_pcm_hw_params_get_format(params, &val);
+    printf("format = '%s' (%s)\n",
+            snd_pcm_format_name((snd_pcm_format_t)val),
+            snd_pcm_format_description(
+                (snd_pcm_format_t)val));
+
+    snd_pcm_hw_params_get_subformat(params,
+            (snd_pcm_subformat_t *)&val);
+    printf("subformat = '%s' (%s)\n",
+            snd_pcm_subformat_name((snd_pcm_subformat_t)val),
+            snd_pcm_subformat_description(
+                (snd_pcm_subformat_t)val));
+
+    snd_pcm_hw_params_get_channels(params, &val);
+    printf("channels = %d\n", val);
+
+    snd_pcm_hw_params_get_rate(params, &val, &dir);
+    printf("rate = %d bps\n", val);
+
+    snd_pcm_hw_params_get_period_time(params,
+            &val, &dir);
+    printf("period time = %d us\n", val);
+
+    snd_pcm_hw_params_get_period_size(params,
+            &frames, &dir);
+    printf("period size = %d frames\n", (int)frames);
+
+    snd_pcm_hw_params_get_buffer_time(params,
+            &val, &dir);
+    printf("buffer time = %d us\n", val);
+
+    snd_pcm_hw_params_get_buffer_size(params,
+            (snd_pcm_uframes_t *) &val);
+    printf("buffer size = %d frames\n", val);
+}
 
 int main(void) {
         int err;
@@ -24,6 +74,9 @@ int main(void) {
                 printf("Playback open error: %s\n", snd_strerror(err));
                 exit(EXIT_FAILURE);
         }
+
+        displayHardwareParameters();
+
         if ((err = snd_pcm_set_params(handle,
                                       SND_PCM_FORMAT_U8,
                                       SND_PCM_ACCESS_RW_INTERLEAVED,
