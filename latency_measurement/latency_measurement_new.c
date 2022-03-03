1013f4e3b5174b49cf7c70900ad7d6fa22fcbaaa
nac62116 <cnash@cnash.de>
2022-03-03 21:49:54 +0100

    Debug: Writing buffer to PCM device

--- latency_measurement/latency_measurement_new.c
+++ latency_measurement/latency_measurement_new.c
@@ -270,7 +270,7 @@ int sendSignalViaPCMDevice(double signalIntervalInS) {
         framesWritten = snd_pcm_writen(pcmHandle, (void **) &interleavedAudioBuffer, sizeof(interleavedAudioBuffer));
     }
     if (framesWritten < 0) {
-        printf("snd_pcm_write failed: %s\n", snd_strerror(framesWritten));
+        fprintf(stderr, "Error writing buffer.\n");
         snd_pcm_recover(pcmHandle, framesWritten, 0);
     }
     // Start measurement
