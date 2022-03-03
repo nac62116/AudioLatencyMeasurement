f423d21c6be6e0578372d90775f3956602e105a0
nac62116 <cnash@cnash.de>
2022-03-03 13:22:39 +0100

    Debugging latency_measurement_new.c

--- latency_measurement/latency_measurement_new.c
+++ latency_measurement/latency_measurement_new.c
@@ -107,7 +107,7 @@ int openPCMDevice() {
 int configurePCMDevice() {
     printf("configurePCMDevice\n");
     if (snd_pcm_hw_params_any(pcmHandle, hwParams) < 0) {
-      fprintf(stderr, "Error configuring PCM device %s\n", pcmName);
+      fprintf(stderr, "Error configuring PCM device \n");
       snd_pcm_close(pcmHandle);
       return(-1);
     }
