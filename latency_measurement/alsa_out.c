dfe0eb4aac06dae28a6ecb078467aa1de3912fe0
nac62116 <cnash@cnash.de>
2022-03-06 14:58:59 +0100

    Debug: Writing buffer to PCM device

--- latency_measurement/alsa_out.c
+++ latency_measurement/alsa_out.c
@@ -14,7 +14,7 @@ const int ALSA_PCM_SOFT_RESAMPLE = 0;
 const unsigned int ALSA_PCM_LATENCY = 0;
 const unsigned int ALSA_PCM_PREFERRED_SAMPLE_RATE = 48000;
 
-char *alsaPcmDevice = "plughw:CARD=usb_audio_top";          /* USB playback device */
+char *alsaPcmDevice = "hw:CARD=usb_audio_top";          /* USB playback device */
 //const char *device = "hw:2,0";        /* HDMI 1 playback device */
 //const char *device = "hw:0,0";        /* HDMI 0 playback device */
 snd_pcm_format_t formatType;
