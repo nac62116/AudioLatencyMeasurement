5eec2a2941fbf87ecbfcebaac81da1569bf36d66
nac62116 <cnash@cnash.de>
2022-03-06 14:47:14 +0100

    Debug: Writing buffer to PCM device

--- latency_measurement/alsa_out.c
+++ latency_measurement/alsa_out.c
@@ -7,7 +7,7 @@ Hardware parameter code base retrieved from https://www.linuxjournal.com/article
 #include <stdio.h>
 
 // BUFFER_SIZE = ALSA_PCM_PREFERRED_SAMPLE_RATE (48000 kHz) * SIGNAL_LENGTH_IN_S (0.001 s)
-#define BUFFER_SIZE 2
+#define BUFFER_SIZE 32
 
 const double SIGNAL_LENGTH_IN_S = 0.001;
 const int ALSA_PCM_SOFT_RESAMPLE = 0;
@@ -133,7 +133,7 @@ void sendSignalViaALSA() {
         }
 
         for (i = 0; i < 10; i++) {
-                for (int j = 0; j < 2400; j++) {
+                for (int j = 0; j < 150; j++) {
                         frames = snd_pcm_writei(handle, buffer, sizeof(buffer));
                         if (frames < 0)
                                 frames = snd_pcm_recover(handle, frames, 0);
