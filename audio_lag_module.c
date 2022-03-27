cae4b5cbbd02c2b0cdced91e983e58b69129d8ec
nac62116 <cnash@cnash.de>
2022-03-28 00:00:59 +0200

    Debugging

--- audio_lag_module.c
+++ audio_lag_module.c
@@ -376,6 +376,27 @@ void prepareExit() {
 // ####
 // #### PCM DEVICES (USB, HDMI, PCIE) VIA ALSA ####
 
+static int xrun_recovery(snd_pcm_t *handle, int err) {
+    if (verbose)
+        printf("stream recovery\n");
+    if (err == -EPIPE) {    /* under-run */
+        err = snd_pcm_prepare(handle);
+        if (err < 0)
+            printf("Can't recovery from underrun, prepare failed: %s\n", snd_strerror(err));
+        return 0;
+    } else if (err == -ESTRPIPE) {
+        while ((err = snd_pcm_resume(handle)) == -EAGAIN)
+            sleep(1);   /* wait until the suspend flag is released */
+        if (err < 0) {
+            err = snd_pcm_prepare(handle);
+            if (err < 0)
+                printf("Can't recover from suspend, prepare failed: %s\n", snd_strerror(err));
+        }
+        return 0;
+    }
+    return err;
+}
+
 void startMeasurementDigitalOut(int measurementMethod) {
     double signalIntervalInS;
     int status;
