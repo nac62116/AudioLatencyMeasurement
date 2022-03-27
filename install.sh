582bb30212d8444b08dfe190fd4a6fae1b41e678
nac62116 <cnash@cnash.de>
2022-03-27 19:37:02 +0200

    Debugging install script

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sudo cp ./11-media-by-label-auto-mount.rules /etc/udev/rules.d/
-sudo cp ./85-my-usb-audio.rules /etc/udev/rules.d/
-chmod +x start_audio_lag_module.sh
+sudo cp ./udev/11-media-by-label-auto-mount.rules /etc/udev/rules.d/
+sudo cp ./udev/85-my-usb-audio.rules /etc/udev/rules.d/
+sudo chmod +x start_audio_lag_module.sh
