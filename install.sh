2e44e4f431c8c218c6925b5c0ef9712e66882145
nac62116 <cnash@cnash.de>
2022-03-27 20:19:29 +0200

    Updated install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 
 sudo cp ./udev/11-media-by-label-auto-mount.rules /etc/udev/rules.d/
 sudo cp ./udev/85-my-usb-audio.rules /etc/udev/rules.d/
+sudo cp ./start_audio_lag_module.sh /etc/profile.d/
 sudo chmod +x start_audio_lag_module.sh
