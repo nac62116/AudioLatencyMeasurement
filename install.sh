da51e7427f298794e4ac204d1468fa3befefba8c
nac62116 <cnash@cnash.de>
2022-03-28 13:14:31 +0200

    Inserted last TODO's

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 sudo cp ./udev/11-media-by-label-auto-mount.rules /etc/udev/rules.d/
 sudo cp ./udev/85-my-usb-audio.rules /etc/udev/rules.d/
-sudo cp ./start_audio_lag_module.sh /etc/profile.d/
+# TODO Insert autostart
+#sudo cp ./start_audio_lag_module.sh /etc/profile.d/
 sudo chmod +x start_audio_lag_module.sh
 sudo udevadm control --reload-rules
